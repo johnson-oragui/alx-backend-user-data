c1a97b1e539f5ff35011aa51062f436182cd4561
sentense1 <3xsaint@gmail.com>
2023-11-06 19:17:30 +0000

    created endpoint: unauthorised

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,11 +2,11 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
@@ -20,9 +20,17 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error):
+    """
+    Handles unauthorised error
+    """
+    response = jsonify({"error" :"Unauthorized"})
+
+    return response, 401
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
-
+    app.run(host=host, port=port, debug=True)
