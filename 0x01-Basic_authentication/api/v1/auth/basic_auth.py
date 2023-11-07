80a0528a0d1bf422ed1bc695caa98784fe2f954e
sentense1 <3xsaint@gmail.com>
2023-11-07 11:25:53 +0000

    Basic - Base64 decode of basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Module for Basic Authentication
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -25,14 +26,31 @@ class BasicAuth(Auth):
             value = authorization_header.split(' ')
             return value[1]
 
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:  # noqa
+        """
+        Returns the decoded value of a Base64 string
+            base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return
+        if not isinstance(base64_authorization_header, str):
+            return
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+        except Exception:
+            return
+        else:
+            return decoded_str
+
 
 if __name__ == "__main__":
     a = BasicAuth()
 
-    print(a.extract_base64_authorization_header(None))
-    print(a.extract_base64_authorization_header(89))
-    print(a.extract_base64_authorization_header("Holberton School"))
-    print(a.extract_base64_authorization_header("Basic Holberton"))
-    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
-    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))  # noqa
-    print(a.extract_base64_authorization_header("Basic1234"))
+    print(a.decode_base64_authorization_header(None))
+    print(a.decode_base64_authorization_header(89))
+    print(a.decode_base64_authorization_header("Holberton School"))
+    print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
+    print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
+    print(a.decode_base64_authorization_header(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))  # noqa
