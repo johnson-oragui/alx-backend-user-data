585db741e1fb40240d87fbf8947896efa061fa38
sentense1 <3xsaint@gmail.com>
2023-11-07 09:49:32 +0000

    created basic_auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for Basic Authentication
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Class that handles basic authentication
+    """
+    pass
