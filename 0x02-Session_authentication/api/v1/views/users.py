bc08f76494f8f4d8e2b3c37f289619a5b8b691b0
sentense1 <3xsaint@gmail.com>
2023-11-09 13:27:55 +0000

    added a GET for user/me

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from api.v1.views import app_views
 from flask import abort, jsonify, request
+from api.v1.views import app_views
 from models.user import User
 
 
@@ -27,6 +27,10 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == "me" and request.current_user is None:
+        abort(404)
+    if user_id == "me" and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -120,4 +124,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
