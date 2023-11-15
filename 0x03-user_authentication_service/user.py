55e456be0ec6140d6ea4c79e011896c53e30b8f6
Johnson Dennis <randommallstudio@gmail.com>
2023-11-15 23:33:33 +0100

    changed varchar to 250

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,10 +15,10 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(255), nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255))
-    reset_token = Column(String(255))
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
 
 
 if __name__ == "__main__":
