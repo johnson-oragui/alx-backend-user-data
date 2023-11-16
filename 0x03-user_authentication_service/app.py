c2de33d94b28af3fd3e8d644dc09d084a913c060
Johnson Dennis <randommallstudio@gmail.com>
2023-11-16 14:44:42 +0100

    added a logout func for DELETE /sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Module to run flask app
 """
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 
@@ -20,6 +20,48 @@ def home():
     message = {"message": "Bienvenue"}
     # Return a JSON response message
     return jsonify(message)
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """
+    Registers Users.
+
+    Handles the registration of users via a POST request.
+    Expects 'email' and 'password' fields in the request form.
+
+    Returns a JSON response with appropriate messages and HTTP status codes.
+
+    HTTP Status Codes:
+    - 200 OK: User successfully registered.
+    - 400 Bad Request: Invalid request method or email already registered.
+    - 500 Internal Server Error: Unexpected error during registration.
+    """
+    # Check if the request method is POST
+    if request.method == "POST":
+        # Get the 'email' field from the request form
+        email = request.form.get("email")
+        # Get the 'password' field from the request
+        password = request.form.get("password")
+        try:
+            # Attempt to register the user using the Auth instance
+            AUTH.register_user(email, password)
+            # Create a JSON response message
+            message = jsonify({"email": email,
+                               "message": "user created"})
+            # Return the JSON response
+            return message
+        # Catch a ValueError if the email is already registered
+        except ValueError:
+            # Return a JSON response with a 400 status code
+            return jsonify({"message": "email already registered"}), 400
+        # Catch other exceptions
+        except Exception as E:
+            # Return a JSON response with a 500 status code
+            return jsonify({"message": f"Error {str(E)} occured"}), 500
+    else:
+        # If the request method is not POST, return a 400 Bad Request
+        abort(400)
 
 
 @app.route("/sessions", methods=["POST"])
@@ -69,46 +111,41 @@ def login():
         abort(401)
 
 
-@app.route("/users", methods=["POST"])
-def users():
+@app.route("/sessions", methods=["DELETE"])
+def logout():
     """
-    Registers Users.
+    Endpoint for user logout.
 
-    Handles the registration of users via a POST request.
-    Expects 'email' and 'password' fields in the request form.
+    Handles user logout by destroying the session associated with the user's
+    session ID. The user is redirected to the home page ("/") after successful
+    logout.
 
-    Returns a JSON response with appropriate messages and HTTP status codes.
-
-    HTTP Status Codes:
-    - 200 OK: User successfully registered.
-    - 400 Bad Request: Invalid request method or email already registered.
-    - 500 Internal Server Error: Unexpected error during registration.
+    Returns:
+    - Redirect to the home page ("/") after successful logout.
+    - HTTP 403 Forbidden if there's an issue during the logout process.
     """
-    # Check if the request method is POST
-    if request.method == "POST":
-        # Get the 'email' field from the request form
-        email = request.form.get("email")
-        # Get the 'password' field from the request
-        password = request.form.get("password")
+
+    # Check if the request method is DELETE
+    if request.method == "DELETE":
+        # Get the user's session ID from the cookies
+        session_id = request.cookies.get("session_id", None)
+
         try:
-            # Attempt to register the user using the Auth instance
-            AUTH.register_user(email, password)
-            # Create a JSON response message
-            message = jsonify({"email": email,
-                               "message": "user created"})
-            # Return the JSON response
-            return message
-        # Catch a ValueError if the email is already registered
-        except ValueError:
-            # Return a JSON response with a 400 status code
-            return jsonify({"message": "email already registered"}), 400
-        # Catch other exceptions
-        except Exception as E:
-            # Return a JSON response with a 500 status code
-            return jsonify({"message": f"Error {str(E)} occured"}), 500
+            # Attempt to get the user associated with the session ID
+            user = AUTH.get_user_from_session_id(session_id)
+
+            # If a user is found, destroy the user's session
+            if user:
+                AUTH.destroy_session(user.id)
+
+            # Redirect the user to the home page after successful logout
+            return redirect("/")
+        except Exception:
+            # Handle exceptions and return HTTP 403 Forbidden
+            abort(403)
     else:
-        # If the request method is not POST, return a 400 Bad Request
-        abort(400)
+        # If the request method is not DELETE, return HTTP 403 Forbidden
+        abort(403)
 
 
 if __name__ == "__main__":
