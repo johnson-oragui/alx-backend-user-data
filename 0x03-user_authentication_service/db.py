f760c177073074373458452201c74675e3c924f7
Johnson Dennis <randommallstudio@gmail.com>
2023-11-16 10:13:14 +0100

    added update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -62,33 +62,62 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         """
         Returns the first row found in the users table as filtered
-            by the method’s input arguments.
+        by the method’s input arguments.
+
+        :param kwargs: Keyword arguments representing filter conditions.
+        :return: User object if found, else raises NoResultFound.
         """
+        # Check if all provided keys in kwargs are valid
+        #   attributes of the User model
         if not all(hasattr(User, key) for key in kwargs.keys()):
             raise InvalidRequestError()
 
+        # Query the database using filter_by to
+        #   filter based on attribute values
         usr = self._session.query(User).filter_by(**kwargs).first()
+
+        # If a user is found, return it; otherwise, raise NoResultFound
         if usr:
             return usr
         raise NoResultFound()
 
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Updates the user attributes based on the provided keyword arguments.
+
+        user_id: ID of the user to be updated.
+        kwargs: User attributes and their new values.
+        :raises ValueError: If an invalid attribute is provided.
+        """
+        try:
+            # Find the user to update based on the provided user_id
+            user_to_update = self.find_user_by(id=user_id)
+
+            # Update user attributes based on keyword arguments
+            for attr, value in kwargs.items():
+                # Check if the attribute is a valid attribute of the User model
+                if hasattr(User, attr):
+                    setattr(user_to_update, attr, value)
+                else:
+                    # Raise a ValueError if an invalid attribute is provided
+                    raise ValueError()
+            # Commit the changes to the database
+            self._session.commit()
+        except NoResultFound:
+            # Raise NoResultFound if no user is found with the specified ID
+            raise NoResultFound()
+
 
 if __name__ == "__main__":
     my_db = DB()
-    user = my_db.add_user("test@test.com", "PwdHashed")
+    email = 'test@test.com'
+    hashed_password = "hashedPwd"
+
+    user = my_db.add_user(email, hashed_password)
     print(user.id)
 
-    find_user = my_db.find_user_by(email="test@test.com")
-    print(find_user.id)
-
     try:
-        find_user = my_db.find_user_by(email="test2@test.com")
-        print(find_user.id)
-    except NoResultFound:
-        print("Not found")
-
-    try:
-        find_user = my_db.find_user_by(no_email="test@test.com")
-        print(find_user.id)
-    except InvalidRequestError:
-        print("Invalid")
+        my_db.update_user(user.id, hashed_password='NewPwd')
+        print("Password updated")
+    except ValueError:
+        print("Error")
